716db3d3fb6d3be1a1e5ad7170698a9cba33ab01
mopsnakak <provllyvovov@gmail.com>
2025-11-03 12:10:53 +0700

    Update ROgram.lua

--- ROgram.lua
+++ ROgram.lua
@@ -1,46 +1,45 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local Window = Rayfield:CreateWindow({
-   Name = "Mops Exploitp",
-   Icon = 0
-   LoadingTitle = "Mops Exploit",
-   LoadingSubtitle = "by Mops",
-   ShowText = "Tgk richmops", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ByteNetUnreliable = ReplicatedStorage:WaitForChild("ByteNetUnreliable")
+local ByteNetReliable = ReplicatedStorage:WaitForChild("ByteNetReliable")
 
-   ToggleUIKeybind = "Wp", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+local Window = Rayfield:CreateWindow({
+   Name = "Mops v.BETA main window",
+   Icon = 0,
+   LoadingTitle = "Mops Hub v.BETA",
+   LoadingSubtitle = "by Mops",
+   ShowText = "Mops Hub v.BETA",
+   Theme = "Default",
+
+   ToggleUIKeybind = "K",
 
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+   DisableBuildWarnings = false,
 
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Mops Hub"
+      FolderName = nil,
+      FileName = "Mops Hub v.BETA"
    },
 
    Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
    },
 
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = false,
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"Hello"}
    }
 })
-Rayfield:Notify({
-   Title = "Notification Title",
-   Content = "Notification Content",
-   Duration = 6.5,
-   Image = "rewind",
-}) 
 local Tab = Window:CreateTab("Auto Click/Fast Click")
 
 local clicking = false
