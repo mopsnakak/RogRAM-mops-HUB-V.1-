1b7e1c8a87a0f5f5129b63aae8e7407758784aae
mopsnakak <provllyvovov@gmail.com>
2025-11-03 11:14:36 +0700

    Mops Hub V.1

---
+++ ROgram.lua
@@ -0,0 +1,59 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Window = Rayfield:CreateWindow({
+   Name = "Mops Exploitp",
+   Icon = 0
+   LoadingTitle = "Mops Exploit",
+   LoadingSubtitle = "by Mops",
+   ShowText = "Tgk richmops", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   ToggleUIKeybind = "Wp", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Mops Hub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+Rayfield:Notify({
+   Title = "Notification Title",
+   Content = "Notification Content",
+   Duration = 6.5,
+   Image = "rewind",
+}) 
+local Tab = Window:CreateTab("Auto Click/Fast Click")
+
+local clicking = false
+Tab:CreateToggle({
+    Name = "Auto Click",
+    CurrentValue = false,
+    Callback = function(Value)
+        clicking = Value
+        task.spawn(function()
+            while clicking do
+                ByteNetUnreliable:FireServer(buffer.fromstring("\21\1"))
+                task.wait()
+            end
+        end)
+    end,
+})
