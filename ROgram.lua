c40b623632d6aa97327c5a56bb1aa71346d6411a
mopsnakak <provllyvovov@gmail.com>
2025-11-03 16:29:27 +0700

    Update ROgram.lua

--- ROgram.lua
+++ ROgram.lua
@@ -1,46 +1,46 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local ByteNetUnreliable = ReplicatedStorage:WaitForChild("ByteNetUnreliable")
-local ByteNetReliable = ReplicatedStorage:WaitForChild("ByteNetReliable")
-
-
 local Window = Rayfield:CreateWindow({
-   Name = "Mops v.BETA main window",
-   Icon = 0,
-   LoadingTitle = "Mops Hub v.BETA",
+   Name = "Mops Exploitp",
+   Icon = 0
+   LoadingTitle = "Mops Exploit",
    LoadingSubtitle = "by Mops",
-   ShowText = "Mops Hub v.BETA",
-   Theme = "Default",
+   ShowText = "Tgk richmops", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
-   ToggleUIKeybind = "K",
+   ToggleUIKeybind = "Wp", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
 
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
 
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil,
-      FileName = "Mops Hub v.BETA"
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Mops Hub"
    },
 
    Discord = {
-      Enabled = false,
-      Invite = "noinvitelink",
-      RememberJoins = true
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
 
-   KeySystem = false,
+   KeySystem = false, -- Set this to true to use our key system
    KeySettings = {
       Title = "Untitled",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key",
-      SaveKey = true,
-      GrabKeyFromSite = false,
-      Key = {"Hello"}
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
-
+Rayfield:Notify({
+   Title = "Notification Title",
+   Content = "Notification Content",
+   Duration = 6.5,
+   Image = "rewind",
+}) 
 local Tab = Window:CreateTab("Auto Click/Fast Click")
 
 local clicking = false
@@ -104,125 +104,3 @@ GiftsTab:CreateToggle({
         end)
     end,
 })
-
-local HeartToggle = false
-GiftsTab:CreateToggle({
-    Name = "Heart spam buy",
-    CurrentValue = false,
-    Callback = function(Value)
-        HeartToggle = Value
-        task.spawn(function()
-            while basketToggle do
-                ByteNetReliable:FireServer(buffer.fromstring("\9\1\0\0\0\5\0Heart"))
-                task.wait(0.05)
-            end
-        end)
-    end,
-}) 
-
-local KeyToggle = false
-GiftsTab:CreateToggle({
-    Name = "InputKey spam buy",
-    CurrentValue = false,
-    Callback = function(Value)
-        KeyToggle = Value
-        task.spawn(function()
-            while basketToggle do
-                ByteNetReliable:FireServer(buffer.fromstring("\9\1\0\0\0\9\0Input Key"))
-                task.wait(0.05)
-            end
-        end)
-    end,
-}) 
-
-local popToggle = false
-GiftsTab:CreateToggle({
-    Name = "LolPop spam buy",
-    CurrentValue = false,
-    Callback = function(Value)
-        popToggle = Value
-        task.spawn(function()
-            while basketToggle do
-                ByteNetReliable:FireServer(buffer.fromstring("\9\1\0\0\0\6\0LolPop"))
-                task.wait(0.05)
-            end
-        end)
-    end,
-}) 
-
-local EyeToggle = false
-GiftsTab:CreateToggle({
-    Name = "Eye spam buy",
-    CurrentValue = false,
-    Callback = function(Value)
-        EyeToggle = Value
-        task.spawn(function()
-            while basketToggle do
-                ByteNetReliable:FireServer(buffer.fromstring("\9\1\0\0\0\3\0Eye"))
-                task.wait(0.05)
-            end
-        end)
-    end,
-}) 
-
-
-local RayderToggle = false
-GiftsTab:CreateToggle({
-    Name = "Low Rayder spam buy",
-    CurrentValue = false,
-    Callback = function(Value)
-        RayderToggle = Value
-        task.spawn(function()
-            while basketToggle do
-                ByteNetReliable:FireServer(buffer.fromstring("\9\1\0\0\0\9\0Low Rider"))
-                task.wait(0.05)
-            end
-        end)
-    end,
-}) 
-
-local CakeToggle = false
-GiftsTab:CreateToggle({
-    Name = "Cake spam buy",
-    CurrentValue = false,
-    Callback = function(Value)
-        CakeToggle = Value
-        task.spawn(function()
-            while basketToggle do
-                ByteNetReliable:FireServer(buffer.fromstring("\9\1\0\0\0\0Homemade Cake")
-                task.wait(0.05)
-            end
-        end)
-    end,
-}) 
-
-
-local BearToggle = false
-GiftsTab:CreateToggle({
-    Name = "Bear spam buy",
-    CurrentValue = false,
-    Callback = function(Value)
-        BearToggle = Value
-        task.spawn(function()
-            while basketToggle do
-                ByteNetReliable:FireServer(buffer.fromstring("\9\1\0\0\0\8\0Toy Bear")
-                task.wait(0.05)
-            end
-        end)
-    end,
-}) 
-
-local dollToggle = false
-GiftsTab:CreateToggle({
-    Name = "Voodoo Doll spam buy",
-    CurrentValue = false,
-    Callback = function(Value)
-        dollToggle = Value
-        task.spawn(function()
-            while basketToggle do
-                ByteNetReliable:FireServer(buffer.fromstring("\9\1\0\0\0\11\0Voodoo Doll")
-                task.wait(0.05)
-            end
-        end)
-    end,
-}) 
